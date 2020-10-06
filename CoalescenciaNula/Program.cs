0745d6c5cd0e53709e63914256c86ec02f1c4623
Samory Sundjata <samory.sundjata@gmail.com>
2020-10-06 17:13:55 -0300

    Add project files.

---
+++ CoalescenciaNula/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CoalescenciaNula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+
+            int? x = Console.Read();
+
+
+            var resultx = x ?? 0;
+
+
+            Console.WriteLine(resultx);
+
+
+            int? y = 10;
+
+
+            var resulty = y ?? 0;
+
+
+            Console.WriteLine(resulty);
+
+            Console.WriteLine("Digite ENTER duas vezes para sair");
+            Console.ReadKey();
+
+        }
+    }
+}
