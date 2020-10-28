c96a4ced7bdd0c60c62f58401a75e7a8d33e4338
Samory Sundjata <samory.sundjata@gmail.com>
2020-10-28 09:41:42 -0300

    Editando Program.cs

--- CoalescenciaNula/Program.cs
+++ CoalescenciaNula/Program.cs
@@ -5,6 +5,7 @@ namespace CoalescenciaNula
     internal class Program
     {
         #region Private Methods
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Escreva Null na linha abaixo e dê Enter:");
